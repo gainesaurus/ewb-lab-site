64aada6669aac6101c410052b500acbcf8c0474a
gainey.michael <gainey.michael@gmail.com>
2023-10-18 15:47:47 -0700

    feat: add navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import Link from 'next/link'
+import navStyles from './navStyles.module.css';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -9,6 +11,11 @@ export const metadata: Metadata = {
   description: 'The website for The University of Western Australia, Kristin Naragon-Gainey and the Emotional Wellbeing Lab',
 }
 
+const links = [
+  {href: '/', label: 'home'},
+  {href: '/about', label: 'about'},
+]
+
 export default function RootLayout({
   children,
 }: {
@@ -16,6 +23,17 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <header>
+        <nav className={navStyles.navbar}>
+          <ul className={navStyles.linkList}>
+            {links.map(link => (
+              <li key={link.href} className={navStyles.navLink}>
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
       <body className={inter.className}>{children}</body>
     </html>
   )
