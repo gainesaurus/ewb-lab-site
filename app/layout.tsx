ec6a68c9a3c340f0a590fe2e4d1712fa47c67938
gainey.michael <gainey.michael@gmail.com>
2023-10-25 10:16:28 -0700

    fix: moved header and footer to body

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,7 @@
 import './globals.css'
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
-import Link from 'next/link'
-import navStyles from './navStyles.module.css'
+
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -11,13 +10,7 @@ export const metadata: Metadata = {
   description: 'The website for The University of Western Australia, Dr. Kristin Naragon-Gainey and the Emotional Wellbeing Lab',
 }
 
-const links = [
-  {href: '/', label: 'home'},
-  {href: '/about', label: 'about'},
-  {href: '/publications', label: 'publications'},
-  {href: '/students', label: 'students'},
-  {href: '/contact', label: 'contact'},
-]
+
 
 export default function RootLayout({
   children,
@@ -26,26 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <header>
-        <nav className={navStyles.navbar}>
-          <img
-            src='http://emotionalwellbeinglab.com.au/wp-content/uploads/2020/09/cropped-Two-logos-1024x152.png' 
-            alt='emotional wellbeing lab logo'
-            className={navStyles.navImg}
-          />
-          <ul className={navStyles.linkList}>
-            {links.map(link => (
-              <li key={link.href} className={navStyles.navLink}>
-                <Link href={link.href}>{link.label}</Link>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </header>
       <body className={inter.className}>{children}</body>
-      <footer>
-        35 Stirling Hwy, Crawley WA 6009 | Phone: (08) 6488 3096 | Email: wellbeinglabUWA@gmail.com
-      </footer>
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,38 @@
 import styles from './page.module.css'
+import Link from 'next/link'
+
+
+const links = [
+  {href: '/', label: 'home'},
+  {href: '/about', label: 'about'},
+  {href: '/publications', label: 'publications'},
+  {href: '/students', label: 'students'},
+  {href: '/contact', label: 'contact'},
+]
 
 export default function Home() {
   return (
     <main className={styles.main}>
+           <header>
+        <nav className={styles.navbar}>
+          <img
+            src='http://emotionalwellbeinglab.com.au/wp-content/uploads/2020/09/cropped-Two-logos-1024x152.png' 
+            alt='emotional wellbeing lab logo'
+            className={styles.navImg}
+          />
+          <ul className={styles.linkList}>
+            {links.map(link => (
+              <li key={link.href} className={styles.navLink}>
+                <Link href={link.href}>{link.label}</Link>
+              </li>
+            ))}
+          </ul>
+        </nav>
+      </header>
      <p>hello</p>
+     <footer>
+        35 Stirling Hwy, Crawley WA 6009 | Phone: (08) 6488 3096 | Email: wellbeinglabUWA@gmail.com
+      </footer>
     </main>
   )
 }
