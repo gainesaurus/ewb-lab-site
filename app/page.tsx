614a4777d397e668ab8afbe9d8691a384464bf83
gainey.michael <gainey.michael@gmail.com>
2023-11-02 19:15:00 -0700

    feat: Current Research list added with styled card

--- app/page.tsx
+++ app/page.tsx
@@ -32,7 +32,7 @@ export default function Home() {
           </ul>
         </nav>
       </header>
-      <div className={styles.homeImgContainer}>
+      <section className={styles.homeImgContainer}>
         <img
           src='https://images.unsplash.com/photo-1538385838701-51d6b218a676?auto=format&fit=crop&q=80&w=2610&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
           alt='Quay Train Station aerial photo'
@@ -42,7 +42,22 @@ export default function Home() {
           <h2>Emotional Wellbeing Lab</h2>
           <button className={styles.button}>GET TO KNOW US</button>
         </div>
-      </div>
+      </section>
+      <section className={styles.researchContainer}>
+        <ul className={styles.researchList}>
+          <label className={styles.researchListTitle}>Current Research</label>
+          <li className={styles.researchListItem}>
+            <img 
+              src='http://emotionalwellbeinglab.com.au/wp-content/uploads/elementor/thumbs/FEEL-Study_logo-e1597123318810-otsnnfaccn5hd7t4u7stsxspvomcq37a9tlm3nbu8c.png'
+              alt='FEEL Study Logo'
+            />
+            <h3>The FEEL Study</h3>
+            <p>How do people respond to their emotions in daily life?</p>
+            <p>How does this impact the way they are feeling and the symptoms they experience?</p>
+            <button className={styles.button}>Learn More</button>
+          </li>
+        </ul>
+      </section>
       <footer>
         35 Stirling Hwy, Crawley WA 6009 | Phone: (08) 6488 3096 | Email: wellbeinglabUWA@gmail.com
       </footer>
